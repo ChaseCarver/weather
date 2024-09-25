90cd8dd61c4e653d05f565de4bdac7aa9af203c8
Chase Carver <chasewcarver@gmail.com>
2024-09-25 10:58:24 -0500

    all 40 weather forcasts displayed in a grid time, temp, and icon

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import React, { useState, useEffect } from 'react';
 function App() {
   const [data, setData] = useState(null);
   const [location, setLocation] = useState(null);
-  
+  const temperature = []
+
   useEffect(() => {
     if(navigator.geolocation){
       navigator.geolocation.getCurrentPosition(
@@ -20,27 +21,41 @@ function App() {
     const [lat, lon] = location
     const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=42d6bde5f66700148deb5a2f62801b36`
     const forecastApi = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=42d6bde5f66700148deb5a2f62801b36&units=imperial`
+    
     fetch(forecastApi)
       .then(response => response.json())
       .then(data => {console.log("Data received:", data); setData(data)})
       .catch(error => console.error("Fetch error:", error));
       console.log("Fetching data from API:", forecastApi)}
   }, [location])
-  
 
-  function kToFh(data){
-    return Math.round((((data - 273.15) * 9/5 + 32) * 100) / 100)
+  function formatDate(date){
+    const options = {
+      month: 'short',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+      hour12: true}
+    return new Date(date).toLocaleString(undefined, options)
   }
+  
+  // if(data){.forEach((element) => 
+  //   temperature.push(`<p>${formatDate(new Date(element.dt * 1000))}</p>` + 
+  //   `<p> ${element.main.temp}°F  </p>` +
+  //   `<img src={https://openweathermap.org/img/wn/${element.weather[0].icon}@2x.png}></img>`))
+  // }
 
   return (
     <div>
       {data ? (
         <>
-          <p>Time: {new Date(data.list[1].dt * 1000).toString()}</p>
-          <p>Current Temperature Is: {data.list[0].main.temp}</p>
-          <p>Current Weather Is: {data.list[0].weather[0].main}, {data.list[0].weather[0].description}</p>
-          <img src={`https://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@2x.png`}></img>
-          <p>Length of list: {data.list.length}</p>
+          {data.list.map((element) => (
+            <div style={{display: 'inline-block', border: '1px solid'}}>
+              <p>{formatDate(new Date(element.dt * 1000))}</p>
+              <p> {element.main.temp}°F  </p>
+              <img width="50" src={`https://openweathermap.org/img/wn/${element.weather[0].icon}@2x.png`}></img>
+            </div>
+            ))}
         </>) : (<p>Loading...</p>)}
     </div>
   );
